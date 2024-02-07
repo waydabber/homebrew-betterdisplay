dc44c6af86c5562c5ed25378690b02fde746bcbe
waydabber <istvan.toth@gmail.com>
2024-02-07 19:02:31 +0100

    Create cli.rb

---
+++ Formula/cli.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Betterdisplaycli < Formula
+  desc "BetterDisplay CLI proxy"
+  homepage "https://github.com/waydabber/betterdisplaycli"
+  url "https://github.com/waydabber/betterdisplaycli.git",
+      tag: "v1.0.0"
+  head "https://github.com/waydabber/betterdisplaycli.git"
+  license "MIT"
+
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "make install prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/betterdisplaycli"
+  end
+end
